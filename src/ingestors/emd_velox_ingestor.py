fe334c379d47f57c921540ce7ca416be2d2128b7
Morgan Wall <mkywall3@gmail.com>
2026-05-11 16:57:12 -0700

    replace all nano-cruc deprecated functions

--- src/ingestors/emd_velox_ingestor.py
+++ src/ingestors/emd_velox_ingestor.py
@@ -318,7 +318,7 @@ class VeloxEmdIngestor(CrucibleDatasetIngestor):
                 # file_to_upload = self.files_to_upload[0] <- INCLUDE if upload_file
             )
         
-            resp = client.create_new_dataset(
+            resp = client.datasets.create(
                 child_ds,
                 scientific_metadata=md,
                 keywords=self.keywords,
@@ -327,12 +327,12 @@ class VeloxEmdIngestor(CrucibleDatasetIngestor):
 
             # add thumbnail for child if applicable
             if child_thumbnail is not None:
-                resp = client.add_thumbnail(child_dsid, child_thumbnail)
+                resp = client.datasets.add_thumbnail(child_dsid, child_thumbnail)
                 logger.info(f'{child_dsid} -- thumbnail -- {resp}')
             else: 
                 logger.info(f'{child_dsid} thumbnail was None')
             # Link child with parent dataset 
-            client.link_datasets(parent_dsid, child_dsid)
+            client.datasets.link_parent_child(parent_dsid, child_dsid)
 
             return child_dsid
         
