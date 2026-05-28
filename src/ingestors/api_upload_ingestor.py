5c54515f9bf778300dc81ef76b15365ea931c0e3
roncofaber <fabrice.roncoroni@gmail.com>
2026-05-28 15:46:56 -0700

    fix: remove sha256_hash_file_to_upload — field no longer on Dataset model

--- src/ingestors/api_upload_ingestor.py
+++ src/ingestors/api_upload_ingestor.py
@@ -1,7 +1,6 @@
 
 import os
 import logging
-from crucible.utils.io import checkhash
 from .crucible_ingestor import CrucibleDatasetIngestor
 
 logger = logging.getLogger(__name__)
@@ -16,8 +15,5 @@ class ApiUploadIngestor(CrucibleDatasetIngestor):
         logger.info(f'{self.unique_id} getting dataset metadata..')
         self.size = os.path.getsize(self.file_to_upload)
         logger.info(f'{self.unique_id} size = {self.size}')
-        if not self.sha256_hash_file_to_upload:
-            self.sha256_hash_file_to_upload = checkhash(self.file_to_upload)
-        logger.info(f'{self.unique_id} hash = {self.sha256_hash_file_to_upload}')
 
             
