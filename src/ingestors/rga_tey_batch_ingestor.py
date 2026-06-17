12821cf74d5a5fe6356db6212f54ee9fa94f503d
Morgan Wall <mkywall3@gmail.com>
2026-06-17 10:30:03 -0700

    not a list now

--- src/ingestors/rga_tey_batch_ingestor.py
+++ src/ingestors/rga_tey_batch_ingestor.py
@@ -66,7 +66,7 @@ class RgaTeyBatchIngestor(CrucibleDatasetIngestor):
         
         samples_in_project= client.samples.list(project_id = '10k_perovskites', sample_type = 'thin film', limit = 10000)
         samples_by_name = {sample["sample_name"]: sample for sample in samples_in_project}
-        logger.info(f'{samples_by_name[0]=}... {len(samples_by_name)=}')
+       # logger.info(f'{samples_by_name[0]=}... {len(samples_by_name)=}')
         df = build_sample_table(extract_path, samples_by_name)
         self.scientific_metadata["samples"] = df.set_index("sample spot").to_dict(orient="index")
     
